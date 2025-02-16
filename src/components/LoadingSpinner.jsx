79120e84839d1eba8851d183cead882481c1ee18
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-17 00:49:52 +0100

    updated styling

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
   return (
-    <div className="fixed inset-0 flex justify-center items-center h-screen bg-gray-900/60 z-50">
+    <div className="fixed inset-0 flex justify-center items-center h-screen bg-gray-900/50 z-60">
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-blue-600"></div>
     </div>
   );
