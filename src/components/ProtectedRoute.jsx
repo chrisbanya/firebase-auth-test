2dbfb5ecd8f01291911f6575884cb0768342c447
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-17 00:51:17 +0100

    added loading spinner

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,29 +2,24 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase/config";
 import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
 
-export default function ProtectedRoute({children}) {
-const [user, setUser] = useState(null)
-const [isLoading, setIsLoading] = useState(true)
+export default function ProtectedRoute({ children }) {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-
-useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, ( user) => {
-        setUser(user)
-        setIsLoading(false)
-    })
-    return () => unsubscribe()
-}, [])
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
-    {/* todo: convert the loading spinner to a reusable components */}
-      {isLoading && (
-        <div className="flex justify-center items-center h-screen">
-          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-blue-600"></div>
-        </div>
-      )}
-      {user ? children : !isLoading && <Navigate to="/signIn" replace/> }
+      {isLoading && <LoadingSpinner />}
+      {user ? children : !isLoading && <Navigate to="/signIn" replace />}
     </>
   );
 }
