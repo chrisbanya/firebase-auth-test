9e60c485999230554e855fd71b7487a266ae0fc7
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-18 13:33:02 +0100

    added signOut button

--- src/Pages/View.jsx
+++ src/Pages/View.jsx
@@ -1,8 +1,20 @@
 import { auth } from "../firebase/config"
+import SignOut from "../components/SignOut"
 export default function View() {
+
   return (
-    <div>
+    <div className="w-5/6 mx-auto mt-4">
+      <nav className="flex justify-between">
+        <div>Logo</div>
+        <div>
+          <SignOut/>
+        </div>
+
+      </nav>
+      <main className="mt-3">
+
         <p>if you viewed this {auth.currentUser?.email} you are signed in</p>
+      </main>
     </div>
   )
 }
