46d45ea59eb9c4e4a38130c641999cbb9b988a7f
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-18 13:32:27 +0100

    added signout logic

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,20 @@
-export default function Button({ children }) {
+import { useState } from "react";
+
+export default function Button({ children, clickEvent }) {
+  const [buttonType, setButttonType] = useState("submit");
+  function handleClick() {
+    if (clickEvent) {
+      setButttonType((prevType) =>
+        prevType === "submit" ? "button" : "submit"
+      );
+      // calls my handleSignout function in SignOut comp..
+      clickEvent();
+    }
+  }
   return (
     <button
-      type="submit"
+      type={buttonType}
+      onClick={handleClick}
       className="w-full shadow-xl py-2.5 px-4 text-sm tracking-wider font-semibold rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
     >
       {children}
