e86fd0ab0f5abda8047f840954fe507984d17b50
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-20 00:16:31 +0100

    refactoring

--- src/components/SignOut.jsx
+++ src/components/SignOut.jsx
@@ -1,20 +1,31 @@
 import { auth } from "../firebase/config";
 import { signOut } from "firebase/auth";
 import Button from "./Button";
+import { useState } from "react";
 
 export default function SignOut() {
+  const [isLoading, setIsLoading] = useState(false);
   function handleSignOut() {
+    setIsLoading(true);
     signOut(auth)
       .then(() => {
         // Sign-out successful.
+        setIsLoading(false);
       })
       .catch((error) => {
         // An error happened.
+        setIsLoading(false);
       });
   }
   return (
     <>
-      <Button clickEvent={handleSignOut}>SignOut</Button>
+      <Button clickEvent={handleSignOut}>
+        {isLoading ? (
+          <span className="animate-pulse">Loading...</span>
+        ) : (
+          "SignOut"
+        )}
+      </Button>
     </>
   );
 }
