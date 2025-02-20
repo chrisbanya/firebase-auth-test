47f16c0609e8341fdae967afa54b3b1dc0b6ac72
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-20 01:03:27 +0100

    added react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import {
   Navigate,
 } from "react-router-dom";
 import SignIn from "./Pages/SignIn";
-import Register from "./Pages/Register";
-import PasswordReset from "./Pages/PasswordReset";
-import View from "./Pages/View";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/ErrorFallback";
+import { Suspense, lazy } from "react";
+import LoadingSpinner from "./components/LoadingSpinner";
 
-// implement lazy loading
-
+const Register = lazy(() => import("./Pages/Register"));
+const PasswordReset = lazy(() => import("./Pages/PasswordReset"));
+const View = lazy(() => import("./Pages/View"));
 
 export default function App() {
   return (
@@ -22,14 +22,30 @@ export default function App() {
         <ErrorBoundary FallbackComponent={ErrorFallback}>
           <Routes>
             <Route path="/signIn" element={<SignIn />} />
-            <Route path="/register" element={<Register />} />
             <Route path="/" element={<Navigate to="/signIn" />} />
-            <Route path="passwordReset" element={<PasswordReset />} />
+            <Route
+              path="/register"
+              element={
+                <Suspense fallback={<LoadingSpinner />}>
+                  <Register />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/passwordReset"
+              element={
+                <Suspense fallback={<LoadingSpinner />}>
+                  <PasswordReset />
+                </Suspense>
+              }
+            />
             <Route
               path="/view"
               element={
                 <ProtectedRoute>
-                  <View />
+                  <Suspense fallback={<LoadingSpinner />}>
+                    <View />
+                  </Suspense>
                 </ProtectedRoute>
               }
             />
